aa96fc4b03d0891216e08ad3385b05103377ac70
Alexis1 <2302117@utrivieramaya.edu.mx>
2024-10-10 08:43:08 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navItem = document.querySelectorAll('.nav-item');
+    navItem.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('show');
+        }, 100 * index);
+    })
+});
