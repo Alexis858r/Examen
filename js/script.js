7e4885a84991b364e96e95df505b75d5cd84d09d
Alexis1 <2302117@utrivieramaya.edu.mx>
2024-10-17 09:14:32 -0600

    examen2

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 100 * index);
     })
 });
+
+const value = document.querySelector('#range_value');
+const input = document.querySelector('#range');
+value.textContent = input.value;
+input.addEventListener ("input", (event) => (
+    value.textContent = event.target.value
+));
